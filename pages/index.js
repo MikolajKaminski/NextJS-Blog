9ebc80bd0a3f3b86bec0e69195cc3c85f8597d55
MikolajKaminski <kola777@yahoo.com>
2020-10-12 12:31:28 +0200

    Added links, assets, metadata and CSS

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,17 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>NextJS Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Read <Link href="/posts/first-post"><a>the first post!</a></Link>
         </h1>
 
         <p className="description">
